cbf7a56d0ad57bbd6b0c13e9c831703f34949897
gilsisou <sisougil@gmail.com>
2020-06-04 02:09:07 +0300

    structured streaming

--- DataCleansing.py
+++ DataCleansing.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from configparser import ConfigParser
-from kafka import KafkaConsumer
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
 import re
 
 config = ConfigParser()
@@ -8,19 +9,34 @@ config.read('configuration.ini')
 kafka_host = config["kafka"]["kafka-host"]
 kafka_port = config["kafka"]["kafka-port"]
 twitter_to_kafka_topic = config['kafka']['twitter_to_kafka_topic']
-
-consumer = KafkaConsumer(twitter_to_kafka_topic, bootstrap_servers=[f'{kafka_host}:{kafka_port}'])
-
-spark = SparkSession.builder.appName("DataCleansing").getOrCreate()
-sc = spark.sparkContext
+after_data_cleansing_kafka = config['kafka']['after_data_cleansing_kafka']
 
 black_list_contains_words = ['#', '@', 'http://', 'https://']
 
+spark = SparkSession.builder.appName("DataCleansing") \
+    .config("spark.jars",
+            "C:\Spark\spark-2.4.5-bin-hadoop2.7\jars\spark-streaming-kafka-0-10_2.11-2.1.0.jar,"
+            "C:\Spark\spark-2.4.5-bin-hadoop2.7\jars\spark-sql-kafka-0-10_2.11-2.1.0.jar") \
+    .getOrCreate()
 
-def cleansing_message(tweet):
-    return tweet.flatMap(lambda message: message.split(" "))\
-        .filter(lambda word: check_if_message_starts_with_bad_char(word))\
-        .collect()
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", f'{kafka_host}:{kafka_port}') \
+    .option("subscribe", twitter_to_kafka_topic) \
+    .load()
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+df.select(explode(split(df.value, " ")).alias("word"))
+
+ds = df.map(_.toString.getBytes).toDF("value")\
+  .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+  .writeStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", f'{kafka_host}:{kafka_port}') \
+  .option("topic", "after_data_cleansing_kafka") \
+  .start()
+
+
 
 
 def check_if_message_starts_with_bad_char(input_word):
@@ -30,12 +46,12 @@ def check_if_message_starts_with_bad_char(input_word):
     return True
 
 
-for tweet in consumer:
-    print(tweet.value.decode('ascii'))
-    tweet_value = sc.parallelize([tweet.value.decode('ascii').lower()])
-    filtered_tweet_rdd = tweet_value.flatMap(lambda s: s.split(" "))\
-        .filter(lambda word: check_if_message_starts_with_bad_char(word)).collect()
-    for word in filtered_tweet_rdd:
-        cleaning_word = re.sub(r'[^a-zA-Z]+', '', word)
-        print(cleaning_word)
+def clean_word_from_nonalpabetic_chars(word):
+    return re.sub(r'[^a-zA-Z]+', '', word)
+#
+#
+# a = df.flatMap(lambda s: s.split(" ")) \
+#     .filter(check_if_message_starts_with_bad_char) \
+#     .map(clean_word_from_nonalpabetic_chars) \
+#     .collect()
 
